3cc2cc3eda6637904940b41821c5fe4cd5c1928d
anguyen6262 <andrew.nguyen6262@gmail.com>
2023-01-25 12:45:52 -0600

    changed lives

--- Breakout/src/breakout/BreakoutGame.java
+++ Breakout/src/breakout/BreakoutGame.java
@@ -13,11 +13,12 @@ public class BreakoutGame {
     private CanvasWindow canvas;
     private Paddle paddle;
     private Ball ball;
-    private int life = 5;
+    private int life = 3;
     private static int bricksLeft = 100;
 
     /**
-     * Lets the user move the paddle with a mouse. Also makes the ball move, checks for different 
+     * Lets the user move the paddle with a mouse. Also makes the ball move, checks
+     * for different
      * elements the call collided with, and checks win/lose conditions.
      */
     public BreakoutGame() {
@@ -25,18 +26,18 @@ public class BreakoutGame {
         brickManager = new BrickManager(canvas);
         canvas.onMouseMove(event -> paddle.setX(event.getPosition().getX()));
         canvas.animate(event -> {
-            ball.hitPaddle(canvas,paddle);
+            ball.hitPaddle(canvas, paddle);
             ball.hitBrick(canvas, brickManager);
             ball.move(1);
 
-        if(ball.topCollision() || ball.sideCollision()){
-            ball.move(1);
-        } 
-        
-        if(loseGame() || winGame()){
-            canvas.closeWindow();
-        }  
-        });  
+            if (ball.topCollision() || ball.sideCollision()) {
+                ball.move(1);
+            }
+
+            if (loseGame() || winGame()) {
+                canvas.closeWindow();
+            }
+        });
     }
 
     /**
@@ -51,7 +52,7 @@ public class BreakoutGame {
      * Constructs a paddle at (200,500).
      */
     public void createPaddle() {
-        paddle = new Paddle(200,500);
+        paddle = new Paddle(200, 500);
         canvas.add(paddle);
     }
 
@@ -59,35 +60,41 @@ public class BreakoutGame {
      * Constructs a ball at (250, 300).
      */
     public void createBall() {
-        ball = new Ball(300,10,250,300,canvas);      
+        ball = new Ball(300, 10, 250, 300, canvas);
     }
-        
+
     /**
      * Adds 100 bricks, a paddle, and ball to the canvas.
      */
     public void run() {
-        createPaddle(); 
+        createPaddle();
         createGrid();
         createBall();
     }
-    
+
     /**
-     * Checks to see if the user won the game. The user wins if they hit all the bricks on the canvas.
-     * @return true if the bricksLeft variable becomes 0. The bricksLeft variable needs to be manually
-     * changed if the total number of bricks on the canvas is not 100.
+     * Checks to see if the user won the game. The user wins if they hit all the
+     * bricks on the canvas.
+     * 
+     * @return true if the bricksLeft variable becomes 0. The bricksLeft variable
+     *         needs to be manually
+     *         changed if the total number of bricks on the canvas is not 100.
      */
     public boolean winGame() {
-        if (bricksLeft<=0) {
+        if (bricksLeft <= 0) {
             return true;
         }
         return false;
     }
-    
+
     /**
-     * Checks to see if the user lost the game. The user loses the game if the ball touches the bottom
+     * Checks to see if the user lost the game. The user loses the game if the ball
+     * touches the bottom
      * of the canvas 3 times.
-     * @return true if the life variable becomes 0. The bricksLeft variable needs to be manually
-     * changed if the user wants more or less than 3 lives.
+     * 
+     * @return true if the life variable becomes 0. The bricksLeft variable needs to
+     *         be manually
+     *         changed if the user wants more or less than 3 lives.
      */
     public boolean loseGame() {
         if (ball.Reset()) {
@@ -97,16 +104,16 @@ public class BreakoutGame {
         }
 
         if (life < 1) {
-                return true;
-            }
+            return true;
+        }
         return false;
     }
 
     public static void destroyBrick() {
-        bricksLeft-=1;
+        bricksLeft -= 1;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BreakoutGame game = new BreakoutGame();
         game.run();
     }
