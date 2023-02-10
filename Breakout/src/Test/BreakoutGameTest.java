5c043b3e0c29c8b8fe0e6468be532ee9bc78ae87
anguyen6262 <andrew.nguyen6262@gmail.com>
2023-02-09 21:59:34 -0600

    added test for loseGame() method

---
+++ Breakout/src/Test/BreakoutGameTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+import breakout.BreakoutGame;
+import org.junit.jupiter.api.Test;
+import java.beans.Transient;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class BreakoutGameTest {
+
+    @Test
+    void loseGame() {
+        BreakoutGame game = new BreakoutGame();
+        life -= 2;
+        assertFalse(game.loseGame());
+        life -= 1;
+        assertTrue(game.loseGame());
+    }
+}
